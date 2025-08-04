a2d2b97bdfe14be0f1f79bc2b3198a817ad7116e
Sravani <sravanishiva192@gmail.com>
2025-08-04 09:37:05 +0530

    Jenkins react app v1.0

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import Navbar from "./components/NavBar";
+import NavBar from "./components/NavBar";
+import Dashboard from "./pages/Dashboard";
+import EmployeeForm from "./pages/EmployeeForm";
+import Login from "./pages/Login";
+import DepartmentPage from "./pages/DepartmentPage";
+
+const App = () => {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/employee-form" element={<EmployeeForm />} />
+        <Route path="/departments" element={<DepartmentPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+
+export default App;
